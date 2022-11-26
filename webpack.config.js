cbfb0ca8813be08f1feecabd8dcb8924aab538f2
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-11-26 18:36:07 +0900

    webpackを導入し、サンプルコードをコピー。

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: "development",
+
+  entry: "./src/index.js",
+
+  // ローカル開発用環境を立ち上げる
+  // 実行時にブラウザが自動的に localhost を開く
+  devServer: {
+    static: {
+      directory: "./dist",
+    },
+    open: true,
+  },
+};
